a7bc59d4d40151095c63b24a09117ab4d69f9502
Tony <greina@eng.ucsd.edu>
2018-02-10 11:48:49 -0800

    Update setup goofys script

--- setup_goofys.sh
+++ setup_goofys.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
-./goofys dse-cohort3-group5 s3bucket
+# Place goofys in your system path (e.g. /usr/bin/goofys)
+mkdir -p s3bucket_goofys
+goofys --debug_fuse dse-cohort3-group5 s3bucket_goofys
 
